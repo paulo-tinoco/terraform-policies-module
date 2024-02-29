ae7c64574e180820a0d1bcc2c6b5a0d20fd744e0
Paulo Tinoco <paulovictortinoco@gmail.com>
2024-02-29 11:50:07 -0300

    first commit

---
+++ example/main.tf
@@ -0,0 +1,43 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+module "teste" {
+  source = "../"
+
+  region     = data.aws_region.current.name
+  account_id = data.aws_caller_identity.current.account_id
+
+  policies = [
+    {
+      iam_reference = "username-iam"
+      iam_type      = "user"
+      statements = [
+        {
+          actions   = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"]
+          resources = ["arn:aws:s3:::callwe-integration-service/*"]
+        },
+        {
+          actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+          resources = ["arn:aws:logs:us-east-1:123456789012:log-group:/aws/lambda/*"]
+        }
+      ]
+    },
+    {
+      iam_reference = "lambda-role-name"
+      iam_type      = "role"
+      statements = [
+        {
+          actions = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"]
+          resources = [
+            "arn:aws:s3:::callwe-integration-service/*",
+            "arn:aws:s3:::callwe-integration-service-dev/*"
+          ]
+        },
+        {
+          actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+          resources = ["arn:aws:logs:us-east-1:123456789012:log-group:/aws/lambda/*"]
+        }
+      ]
+    },
+  ]
+}
